a5cf9de6b8e14950e8aababd63e1d0921543d82b
wook-jpg <jwook401@gmail.com>
2025-05-23 06:25:40 +0900

    kt파일 수정버전
    Tmap APK에서 앱 이름으로 ainfra가 사용되지 않아서 일단 코드만 수정해서 올립니다

--- android/app/src/main/kotlin/com/example/ai_n_fra/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/ai_n_fra/MainActivity.kt
@@ -1,4 +1,15 @@
 package com.example.ai_n_fra
 
-class MainActivity {
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.embedding.engine.FlutterEngineCache
+
+class MainActivity: FlutterActivity() {
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        val factory = TmapViewFactory(this)
+        flutterEngine.platformViewsController.registry.registerViewFactory("tmap-view-type", factory)
+
+        FlutterEngineCache.getInstance().put("my_engine_id", flutterEngine)
+    }
 }
